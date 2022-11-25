08d9865f2cb8496da37da59d158f0862713b4b16
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-11-26 00:33:40 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g79cke1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.get('/',(req, res)=>{
-    res.send("This is home");
-})
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
-app.listen(port,()=>{
-    console.log("Server is running", port);
-})
+async function run() {
+  try {
+    const productsCollection = client.db("auraro").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("This is home");
+});
+
+app.listen(port, () => {
+  console.log("Server is running", port);
+});
